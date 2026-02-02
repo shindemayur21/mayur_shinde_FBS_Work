e7af9a04aeea65a10acfb657c1804cd6568674d2
shindemayur21 <mayurs210198@gmail.com>
2026-02-02 09:17:55 +0530

    Delete Core_python/Assignment_2 directory

--- Core_python/Assignment_2/q4.py
+++
@@ -1,8 +0,0 @@
-#WAP to calculate area of triangle and rectangle.
-
-l = int(input("Enter lenghth of rectangle : "))
-b = int(input("Enter breadth of rectangle : "))
-
-rectArea = l * b
-
-print(f'Area of rectangle is : {rectArea}')
--- Core_python/Assignment_2/q5.py
+++
@@ -1,9 +0,0 @@
-#WAP to calculate selling price of book based on cost price and discount.
-
-cp = float(input("Enter cost price : "))
-disc = int(input("Enter discount : "))
-
-#calculating selling price after discount
-sp = cp - (cp * (disc/100))
-
-print(f'Selling price after {disc}% discount is : {sp}')
