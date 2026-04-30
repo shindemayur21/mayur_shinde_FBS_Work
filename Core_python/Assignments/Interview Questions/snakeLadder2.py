6a59dd90b210bcfee2b808b6137fa3505a65777f
shindemayur21 <mayurs210198@gmail.com>
2026-04-30 08:23:42 +0530

    Add files via upload

---
+++ Core_python/Assignments/Interview Questions/snakeLadder2.py
@@ -0,0 +1,13 @@
+# Snake Ladder board with list comprehension
+
+
+# li3 = [[ele for ele in range(i * 10 + 1, (i + 1) * 10 + 1) if i % 2 == 0] or
+#        [ele for ele in range((i + 1) * 10, i * 10, -1) if i % 2 != 0]
+#        for i in range(0, 10)]
+
+li3 = [[ele for ele in range(i * 10 + 1, (i + 1) * 10 + 1) if i % 2 == 0] or
+       [ele for ele in range((i + 1) * 10, i * 10, -1) if i % 2 != 0]
+       for i in range(0, 10)]
+
+for ele in li3:
+    print(ele)
